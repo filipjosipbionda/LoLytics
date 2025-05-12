401b91d9d4991938c160b075ed142a2e8cd6a5ac
fjbionda <filip.josip.bionda@gmail.com>
2025-05-13 00:06:58 +0200

    Remove .idea from Git tracking

---
+++ app/src/main/java/com/rma/lolytics/ui/auth/password/ForgotPasswordScreen.kt
@@ -0,0 +1,9 @@
+package com.rma.lolytics.ui.auth.password
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+internal fun ForgotPasswordScreen(
+    modifier: Modifier = Modifier
+)

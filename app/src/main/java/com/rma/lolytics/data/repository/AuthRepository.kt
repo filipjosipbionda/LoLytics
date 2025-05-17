2bd1abe760a702c63efd92d567b2e3f92dc5ed31
fjbionda <filip.josip.bionda@gmail.com>
2025-05-17 16:47:14 +0200

    Fix formatting

--- app/src/main/java/com/rma/lolytics/data/repository/AuthRepository.kt
+++ app/src/main/java/com/rma/lolytics/data/repository/AuthRepository.kt
@@ -2,16 +2,16 @@ package com.rma.lolytics.data.repository
 
 interface AuthRepository {
     suspend fun login(
-            email: String,
-            password: String,
-        ): Result<Unit>
+        email: String,
+        password: String,
+    ): Result<Unit>
 
     fun checkIfUserIsLogged(): Boolean
 
-   suspend fun sendPasswordResetEmail(email: String): Result<Unit>
+    suspend fun sendPasswordResetEmail(email: String): Result<Unit>
 
-   suspend fun register(
-       email: String,
-       password: String,
-   ): Result<Unit>
+    suspend fun register(
+        email: String,
+        password: String,
+    ): Result<Unit>
 }
